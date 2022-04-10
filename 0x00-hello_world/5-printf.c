aef57defd5ecb10ad5aec3a5807b4d987c511001
kalisa sangwa kelly <kalisakelly2001@gmail.com>
2022-04-10 14:56:49 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * my hello, printf task
- * return: return 1 after program execution 
+ *
+ * Return: Always 0 (Success) 
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
