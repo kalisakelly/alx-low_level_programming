ce18b096382cbebbcdc1bfbe7097c34d08bc4661
kalisa sangwa kelly <kalisakelly2001@gmail.com>
2022-04-10 16:37:35 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * return : always 0 (success)
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-write(1."and that piece of art is useful\"-dora korpar, 2015-10-19\n",59 );
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
