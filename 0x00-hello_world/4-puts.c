7b9c5248a56a37244e1f2eeec12f11c92d5548b1
kalisa sangwa kelly <kalisakelly2001@gmail.com>
2022-04-10 15:33:28 -0700

    input

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 
 /**
-  * main - Entry point
-  *
-  * Return: always 0 (success) 
-  */
+* main - Entry point
+*
+* Return: Always 0 (Success) 
+*/
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
-
