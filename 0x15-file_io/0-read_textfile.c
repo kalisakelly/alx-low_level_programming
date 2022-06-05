48f86d5e7f195a7fbc80be6c6cc09e8a96a21d2a
kalisakelly <kalisakelly2001@gmail.com>
2022-06-05 10:12:28 -0700

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,36 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include "main.h"
 /**
- * read_textfile - reads text file and prints it to the
-  *POSIX standard output
-  * @filename: represents a pointer
-  * @letters: the number of letters it should read or print
-  * Return: the actual number of letters it could read and print
-  *0 if filename is NULL, can't be opened, read or write fails
-  *or does not write the expected amount of bytes
+  * read_textfile - reads a text file and prints to the POSIX stdout.
+  * @filename: pointer to name of file.
+  * @letters:  number of letters it should read and print.
+  *
+  * Return: number of letters it could print or write or 0.
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int file, rd, wr;
 	char *buf;
-	ssize_t numRD, numWR;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
 		return (0);
-	numRD = read(fd, buf, letters);
-	numWR = write(STDOUT_FILENO, buf, numRD);
-	close(fd);
+	rd = read(file, buf, letters);
+	if (rd == -1)
+		return (0);
+
+	buf[letters] = '\0';
+
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+		return (0);
+
+	close(file);
 	free(buf);
-	return (numWR);
+	return (wr);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,19 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
+/* library files*/
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+/*function prototypes*/
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_file(int file_from, int file_to, char *argv[])
-int _putchar(char c);
 
-#endif
+#endif /* MAIN_H */
